c589ceb5ce4b89fa9baed777f7e6a8d662ece38d
ElJerry <kanegerardo_@hotmail.com>
2018-02-06 22:08:33 -0600

    Inicio del sistema de analogia, click en los individuos lanza un menu donde aparecera la informacion del individuo

---
+++ Assets/Scripts/Individuo/Analogia.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Analogia : MonoBehaviour {
+
+	public static GameObject canvasGo = null;
+	public Canvas canvas;
+
+	void Start(){
+		if (canvasGo == null) {
+			canvasGo = GameObject.Find ("Canvas");
+			canvasGo.SetActive (false);
+		}
+
+	}
+
+	void OnMouseDown(){
+		print (name + "fue clicado");
+		canvasGo.SetActive(true);
+		Time.timeScale = 0;
+	}
+}
---
+++ Assets/Scripts/Panel/EventosUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventosUI : MonoBehaviour {
+
+	public void continuar(){
+		Time.timeScale = 1f;
+	}
+}
