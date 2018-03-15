b98fc40737adefc148187792da97a52ddfd8c750
Gerardo <kanegerardo_@hotmail.com>
2018-03-15 15:46:56 -0600

    Agrego el menu inicial de la simulacion junto con el script para cargar los niveles

---
+++ Assets/Scripts/ControlMenuInicial.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenuInicial : MonoBehaviour {
+
+	public void CargarNivel(int id){
+		Application.LoadLevel (id);
+	}
+}
